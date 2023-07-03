e1b540945e55cd074f416103b1bc49ef9d351292
Farshid Mehrtash <mehrtash.fm@live.com>
2023-07-03 10:55:54 +0300

    trait to build response response easier

---
+++ app/Http/Controllers/APIResponseHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+trait APIResponseHelper
+{
+    /**
+     * success response method.
+     *
+     * @param $result
+     * @param $message
+     * @return JsonResponse
+     */
+    public function successResponse($result, $message = null): JsonResponse
+    {
+        $response ['success'] = true;
+
+        if (!is_null($result)) {
+            $response['data'] = $result;
+        }
+
+        if ($message) {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response);
+    }
+
+
+    /**
+     * return error response.
+     *
+     * @param $error
+     * @param array $errorMessages
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function errorResponse($error, $errorMessages = [], $code = 404): JsonResponse
+    {
+        $response ['success'] = false;
+
+        if (!is_null($error)) {
+            $response['data'] = $error;
+        }
+
+        if ($errorMessages) {
+            $response['message'] = $errorMessages;
+        }
+
+
+        return response()->json($response, $code);
+    }
+}
