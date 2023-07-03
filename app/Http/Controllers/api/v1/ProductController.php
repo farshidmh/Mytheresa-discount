9091c076317ced02ff1059b55b6494727678edb4
Farshid Mehrtash <mehrtash.fm@live.com>
2023-07-03 17:50:46 +0300

    clean up

--- app/Http/Controllers/api/v1/ProductController.php
+++ app/Http/Controllers/api/v1/ProductController.php
@@ -6,20 +6,42 @@ use App\Actions\Product\ListAllProductsAction;
 use App\Actions\Product\ListProductsByCategoryNameAction;
 use App\Actions\Product\ListProductsByPriceLessThanAction;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
 
-
+/**
+ * Class ProductController
+ * This controller handles all product-related requests.
+ *
+ * @version 1.0.0
+ */
 class ProductController extends Controller
 {
-    private int $perPage = 5;
+    /**
+     * Items per page for pagination
+     */
+    private const PER_PAGE = 5;
 
+    /**
+     * Handles request to fetch all products.
+     *
+     * @param ListAllProductsAction $listProductsAction
+     * @return JsonResponse
+     */
     public function getAllProducts(ListAllProductsAction $listProductsAction)
     {
-        $products = $listProductsAction->execute($this->perPage);
+        $products = $listProductsAction->execute(self::PER_PAGE);
         return $this->successResponse($products, 'Products retrieved successfully.');
     }
 
-    public function getProductsByName(
+    /**
+     * Handles request to fetch products by category name.
+     *
+     * @param string $categoryName
+     * @param ListProductsByCategoryNameAction $listProductsAction
+     * @return JsonResponse
+     */
+    public function getProductsByCategoryName(
         string                           $categoryName,
         ListProductsByCategoryNameAction $listProductsAction
     )
@@ -33,10 +55,17 @@ class ProductController extends Controller
             return $this->errorResponse(null, $validator->errors(), 422);
         }
 
-        $products = $listProductsAction->execute($categoryName, $this->perPage);
+        $products = $listProductsAction->execute($categoryName, self::PER_PAGE);
         return $this->successResponse($products, 'Products retrieved successfully.');
     }
 
+    /**
+     * Handles request to fetch products with a price less than a specified amount.
+     *
+     * @param int $price
+     * @param ListProductsByPriceLessThanAction $byPriceLessThanAction
+     * @return JsonResponse
+     */
     public function getProductsByPrice(
         int                               $price,
         ListProductsByPriceLessThanAction $byPriceLessThanAction
@@ -51,7 +80,7 @@ class ProductController extends Controller
             return $this->errorResponse(null, $validator->errors(), 422);
         }
 
-        $products = $byPriceLessThanAction->execute($price, $this->perPage);
+        $products = $byPriceLessThanAction->execute($price, self::PER_PAGE);
         return $this->successResponse($products, 'Products retrieved successfully.');
 
     }
